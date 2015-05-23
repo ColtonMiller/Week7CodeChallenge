50951350ed72cec66d69805c3a02fd1726f59aac
Colton Miller <ColtonJMMiller@gmail.com>
2015-05-23 14:41:54 -0600

    Set DataContext and webconfig

---
+++ Week7CodeChallenge/Models/ContactDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Week7CodeChallenge.Models
+{
+    public class ContactDataContext : DbContext
+    {
+        public virtual DbSet<W7Contact> Contacts { get; set; }
+    }
+}
---
+++ Week7CodeChallenge/Models/W7Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Week7CodeChallenge.Models
+{
+    public class W7Contact
+    {
+        //properties
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ContactId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public EmailAddressAttribute Email { get; set; }
+        [Required]
+        public PhoneAttribute Phone { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string ProjectDescription { get; set; }
+        [Required]
+        public string Comment { get; set; }
+    }
+}
